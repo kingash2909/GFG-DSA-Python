21ba86a4100d4739f5b02883b8587f1dfad2966c
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-03 14:19:13 +0530

    Soln Updated

---
+++ Search/Searching an element in a sorted array.py
@@ -0,0 +1,15 @@
+class Solution:
+    ##Complete this function
+    def searchInSorted(self,arr, N, K):
+        #Your code here
+        
+        low, high = 0, N - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if arr[mid] == K:
+                return 1
+            elif arr[mid] < K:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
