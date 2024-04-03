1aa18d7625dd78809f06f833a65087cd8527d33f
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-03 15:19:14 +0530

    soln Added

---
+++ String/Anagram.py
@@ -0,0 +1,7 @@
+
+class Solution:
+    
+    #Function is to check whether two strings are anagram of each other or not.
+    def isAnagram(self,a,b):
+        #code here
+        return sorted(a) == sorted(b)
