1d1ba953875a4c252f5bdd57fcb8f17b40f8c992
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-03 13:37:35 +0530

    Recursion Soln Updated

---
+++ Recursion/Print 1 To N Without Loop.py
@@ -0,0 +1,9 @@
+
+class Solution:    
+    #Complete this function
+    def printNos(self,N):
+        #Your code here
+        
+        if N>0:
+            self.printNos(N-1)
+            print(N, end=" ")
