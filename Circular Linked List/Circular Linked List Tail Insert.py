847d5460063abd6afcd1ce1cee9fed4f11ba4540
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-05 13:26:30 +0530

    Code added

---
+++ Circular Linked List/Circular Linked List Tail Insert.py
@@ -0,0 +1,26 @@
+
+'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+'''
+
+def insertInTail(head,x):
+    #code here
+    new_node = Node(x)
+
+    if head is None:  # If list is empty, new node becomes the head itself
+        new_node.next = new_node
+        return new_node
+
+    # Traverse to the last node of the list
+    last = head
+    while last.next != head:
+        last = last.next
+
+    # Insert new node after the tail
+    new_node.next = head
+    last.next = new_node
+
+    return head  # Head remains unchanged
