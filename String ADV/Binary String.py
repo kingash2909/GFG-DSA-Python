552a1a9c14df146adb94cb23cbf94faaef185b12
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-16 13:02:47 +0530

    Code Added

---
+++ String ADV/Binary String.py
@@ -0,0 +1,12 @@
+
+class Solution:
+    
+    #Function to count the number of substrings that start and end with 1.
+    def binarySubstring(self,n,s):
+        #code here
+
+        count = 0  
+        for i in range(n):
+            if s[i] == '1':
+                count += 1
+        return (count * (count - 1)) // 2 
