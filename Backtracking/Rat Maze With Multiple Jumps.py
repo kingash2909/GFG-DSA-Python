38bf5e174e9fc3c94942edb8ab5b7a64b67eeef4
kingash2909 <i.m.ashishhh@gmail.com>
2024-04-16 18:46:56 +0530

    Code Updated

--- Backtracking/Rat Maze With Multiple Jumps.py
+++ Backtracking/Rat Maze With Multiple Jumps.py
@@ -2,17 +2,25 @@
 class Solution:
 	def ShortestDistance(self, matrix):
 		# Code here
-        if i==m-1 and j==m-1:
-            return True
-        
-        if a[i][j]!=0:
-            for k in range(1,a[i][j]+1):
-                if j+k<m:
-                    r[i][j+k]=1
-                    if self.maze(a,m,r,i,j+k):return True
-                    r[i][j+k]=0
-                if i+k<m:
-                    r[i+k][j]=1
-                    if self.maze(a,m,r,i+k,j):return True
-                    r[i+k][j]=0
-        return False
+        n = len(matrix)
+        MAX = 10**9+7
+        ans = [[0]*n for i in range(n)]
+
+        def go(i, j):
+                if i == n-1 and j == n-1:
+                    ans[i][j] = 1
+                    return True
+                if i >= n or j >= n or matrix[i][j] == 0:
+                    return False
+                jump = matrix[i][j]
+                for t in range(1, jump+1):
+                    if go(i, j+t):
+                        ans[i][j] = 1
+                        return True
+                    if go(i+t, j):
+                        ans[i][j] = 1
+                        return True
+                return False
+        if go(0, 0):
+            return ans
+        return [[-1]]
